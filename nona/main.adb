d21c56574892feebbf99b69866df2913a90085a7
Joao <victor-souza1997@hotmail.com>
2023-06-27 07:20:13 -0400

    works

--- nona/main.adb
+++ nona/main.adb
@@ -3,12 +3,9 @@ with Ada.Text_IO; use Ada.Text_IO;
 
 
 procedure Main is
-
-   package I is new queuemod (Element => Integer);
-
+   
 begin
-   I.Insert(1);
-   I.Insert(2);
+   queuemod.Insert(1);
    --I.Remove(0);
    Put_Line ("Value is initialized");
 
--- nona/queuemod.ads
+++ nona/queuemod.ads
@@ -1,10 +1,11 @@
 
-generic type Element is private;
-
 
 package queuemod is
--- assuma que o tipo Element está no escopo
-function Empty return Boolean;
-    procedure Insert (E : Element);
-    procedure Remove (E : out Element);
+
+   type Element;
+   type Element is new  Integer;
+   function Empty return Boolean;
+   procedure Insert(E: Element);
+   procedure Remove(E: out Element);
+   
 end queuemod;
